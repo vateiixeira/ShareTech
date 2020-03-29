c09062ae416d5e7d894bfcbb450a635e5b1fe092
Vinicius Alexandre <vateiixeira@gmail.com>
2020-03-29 20:37:21 -0300

    mods

--- sharetech/settings.py
+++ sharetech/settings.py
@@ -14,6 +14,10 @@ import os
 import cloudinary  # cloudinary
 import cloudinary.uploader  # cloudinary
 import cloudinary.api  # cloudinary
+import django_heroku
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
